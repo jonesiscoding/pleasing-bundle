fd5726c0d29551aa770cfdad904c7958e71ce9d5
AMJones <am@jonesiscoding.com>
2025-04-30 18:22:28 -0500

    Updates SassProcessor to extend FileProcessor

--- src/Processor/SassProcessor.php
+++ src/Processor/SassProcessor.php
@@ -2,17 +2,17 @@
 
 namespace DevCoding\Pleasing\Processor;
 
-use DevCoding\Pleasing\Asset\FileAsset;
-use DevCoding\Pleasing\Asset\InputCollection;
-use DevCoding\Pleasing\Handler\SassHandler;
+use DevCoding\Pleasing\Handler\HandlerInterface;
+use DevCoding\Pleasing\Handler\SassHandlerTrait;
 use DevCoding\Pleasing\Locators\SassLocator;
 use DevCoding\Pleasing\Config\Config;
-use DevCoding\Pleasing\Config\AssetConfig;
 
-class SassProcessor extends SassHandler implements ProcessorInterface
+/**
+ * @property SassLocator $Locator
+ */
+class SassProcessor extends FileProcessor implements HandlerInterface, ProcessorInterface
 {
-  protected Config      $Config;
-  protected SassLocator $Locator;
+  use SassHandlerTrait;
 
   /**
    * @param Config           $Config
@@ -20,47 +20,6 @@ class SassProcessor extends SassHandler implements ProcessorInterface
    */
   public function __construct(Config $Config, ?SassLocator $Locator = null)
   {
-    $this->Config  = $Config;
-    $this->Locator = $Locator ?? new SassLocator($this->Config);
-  }
-
-  /**
-   * @param AssetConfig $Asset
-   * @param bool        $forceGen
-   *
-   * @return AssetConfig
-   */
-  public function process(AssetConfig $Asset, bool $forceGen = true): AssetConfig
-  {
-    if ($forceGen || !$Asset->isFresh())
-    {
-      $inputs = $Asset->inputs;
-      if (!empty($inputs))
-      {
-        $collection = new InputCollection($inputs, $this->Locator);
-        $content    = $collection->getContent();
-
-        if (!empty($content))
-        {
-          if ($filters = $Asset->filters)
-          {
-            $FilterCollection = $this->Config->filters;
-            $FilterAsset      = (new FileAsset($Asset->output))->withContent($content);
-            foreach($filters as $filterId)
-            {
-              $Filter = $FilterCollection->get($filterId);
-              $FilterAsset->loadFilter($Filter);
-              $FilterAsset->applyFilter($Filter);
-            }
-
-            $content = $FilterAsset->getContent();
-          }
-
-          $Asset->write($content, $collection->getResources());
-        }
-      }
-    }
-
-    return $Asset;
+    parent::__construct($Config, $Locator ?? new SassLocator($Config));
   }
 }
